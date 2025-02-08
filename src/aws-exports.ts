b1481cbc32890012cbe851187659622ada9a06ea
matt <mattschwartz@users.noreply.github.com>
2025-02-07 18:35:42 -0600

    accidentally deployed a local endpoint

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -11,8 +11,8 @@ const awsExports: ResourcesConfig = {
     REST: {
       'BCGamesServiceAPI': {
         service: "BCGamesServiceAPI",
-        endpoint: "http://localhost:9000/local", // This is the local endpoint
-        // endpoint: "https://e7oyonnbha.execute-api.us-east-1.amazonaws.com/main",
+        // endpoint: "http://localhost:9000/local", // This is the local endpoint
+        endpoint: "https://e7oyonnbha.execute-api.us-east-1.amazonaws.com/main",
         region: "us-east-1",
       }
     }
